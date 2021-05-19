5507d58cab824bd09b1c834054427cfa5f4a2e13
eaj4 <70153018+eaj4@users.noreply.github.com>
2021-05-19 18:55:34 -0400

    Add atrributes to Ball
    add attributes to Ball class

--- app/src/main/java/com/onlineschool/pong/Ball.java
+++ app/src/main/java/com/onlineschool/pong/Ball.java
@@ -1,4 +1,16 @@
 package com.onlineschool.pong;
+import android.graphics.RectF;
 
 public class Ball {
+
+    // These are the member variables (fields)
+    // They all have the m prefix
+    // They are all private
+    // because direct access is not required
+    private RectF mRect;
+    private float mXVelocity;
+    private float mYVelocity;
+    private float mBallWidth;
+    private float mBallHeight;
+
 }
