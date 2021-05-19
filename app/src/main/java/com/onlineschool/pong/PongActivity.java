94f919b1ca1e1d518ec7e178d651888d6db00fb8
eaj4 <70153018+eaj4@users.noreply.github.com>
2021-05-19 14:02:49 -0400

    refactor MainActivity
    changed to PongActivity

--- app/src/main/java/com/onlineschool/pong/PongActivity.java
+++ app/src/main/java/com/onlineschool/pong/PongActivity.java
@@ -3,7 +3,7 @@ package com.onlineschool.pong;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PongActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
