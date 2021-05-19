42ba5db1a518780af3b6af8a0c81fbf592a7c0aa
eaj4 <70153018+eaj4@users.noreply.github.com>
2021-05-19 19:09:19 -0400

    update Bat
    add bat attributes

--- app/src/main/java/com/onlineschool/pong/Bat.java
+++ app/src/main/java/com/onlineschool/pong/Bat.java
@@ -1,4 +1,27 @@
 package com.onlineschool.pong;
 
+import android.graphics.RectF;
+
 public class Bat {
+
+    // These are the member variables (fields)
+    // They all have the m prefix
+    // They are all private
+    // because direct access is not required
+    private RectF mRect;
+    private float mLength;
+    private float mXCoord;
+    private float mBatSpeed;
+    private int mScreenX;
+    // These variables are public and final
+    // They can be directly accessed by
+    // the instance (in PongGame)
+    // because they are part of the same
+    // package but cannot be changed
+    final int STOPPED = 0;
+    final int LEFT = 1;
+    final int RIGHT = 2;
+    // Keeps track of if and how the bat is moving
+    // Starting with STOPPED condition
+    private int mBatMoving = STOPPED;
 }
