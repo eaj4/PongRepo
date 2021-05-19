3dbc944e55aa083ef2ea77d242dd3ab9fee111fb
eaj4 <70153018+eaj4@users.noreply.github.com>
2021-05-19 18:56:28 -0400

    Ball constructor
    create ball constructor

--- app/src/main/java/com/onlineschool/pong/Ball.java
+++ app/src/main/java/com/onlineschool/pong/Ball.java
@@ -13,4 +13,24 @@ public class Ball {
     private float mBallWidth;
     private float mBallHeight;
 
+    // This is the constructor method.
+    // It is called by the code:
+    // mBall = new Ball(mScreenX);
+    // In the PongGame class
+    public Ball(int screenX){
+
+        // Make the ball square and 1% of screen width
+        // of the screen width
+        mBallWidth = screenX / 100;
+        mBallHeight = screenX / 100;
+
+        // Initialize the RectF with 0, 0, 0, 0
+        // We do it here because we only want to
+        // do it once.
+        // We will initialize the detail
+        // at the start of each game
+        mRect = new RectF();
+
+    }
+
 }
