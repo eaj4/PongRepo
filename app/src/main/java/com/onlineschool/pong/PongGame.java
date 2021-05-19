0af5d8d1dd60dec668763455958a71766b8e9846
eaj4 <70153018+eaj4@users.noreply.github.com>
2021-05-19 18:40:39 -0400

    thread Variables
    added thread variables

--- app/src/main/java/com/onlineschool/pong/PongGame.java
+++ app/src/main/java/com/onlineschool/pong/PongGame.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -11,27 +12,35 @@ public class PongGame extends SurfaceView implements Runnable {
 
     // Are we debugging?
     private final boolean DEBUGGING = true;
-// These objects are needed to do the drawing
+    // These objects are needed to do the drawing
     private SurfaceHolder mOurHolder;
     private Canvas mCanvas;
     private Paint mPaint;
-// How many frames per second did we get?
+    // How many frames per second did we get?
     private long mFPS;
-// The number of milliseconds in a second
+    // The number of milliseconds in a second
     private final int MILLIS_IN_SECOND = 1000;
-// Holds the resolution of the screen
+    // Holds the resolution of the screen
     private int mScreenX;
     private int mScreenY;
-// How big will the text be?
+    // How big will the text be?
     private int mFontSize;
     private int mFontMargin;
-// The game objects
+    // The game objects
     private Bat mBat;
     private Ball mBall;
-// The current score and lives remaining
+    // The current score and lives remaining
     private int mScore;
-
     private int mLives;
+    // Here is the Thread and two control variables
+    private Thread mGameThread = null;
+    // This volatile variable can be accessed
+    // from inside and outside the thread
+    private volatile boolean mPlaying;
+    private boolean mPaused = true;
+
+
+
 
     // The PongGame constructor
     // Called when this line:
@@ -93,6 +102,35 @@ public class PongGame extends SurfaceView implements Runnable {
     // from inside and outside the thread
         private volatile boolean mPlaying;
         private boolean mPaused = true;
+
+    }
+
+    // This method is called by PongActivity
+    // when the player quits the game
+    public void pause() {
+
+        // Set mPlaying to false
+        // Stopping the thread isn't
+        // always instant
+        mPlaying = false;
+
+        try {
+            // Stop the thread
+            mGameThread.join();
+        } catch (InterruptedException e) {
+            Log.e("Error:", "joining thread");
+        }
+    }
+
+    // This method is called by PongActivity
+    // when the player starts the game
+    public void resume() {
+
+        mPlaying = true;
+        // Initialize the instance of Thread
+        mGameThread = new Thread(this);
+        // Start the thread
+        mGameThread.start();
 
     }
 
