2b53766b14c3ec6eed7d248b513a69979d399f83
eaj4 <70153018+eaj4@users.noreply.github.com>
2021-05-19 19:10:17 -0400

    Bat.java
    added bat constructor

--- app/src/main/java/com/onlineschool/pong/Bat.java
+++ app/src/main/java/com/onlineschool/pong/Bat.java
@@ -24,4 +24,36 @@ public class Bat {
     // Keeps track of if and how the bat is moving
     // Starting with STOPPED condition
     private int mBatMoving = STOPPED;
+
+    public Bat(int sx, int sy){
+
+        // Bat needs to know the screen
+        // horizontal resolution
+        // Outside of this method
+        mScreenX = sx;
+
+        // Configure the size of the bat based on
+        // the screen resolution
+        // One eighth the screen width
+        mLength = mScreenX / 8;
+
+        // One fortieth the screen height
+        float height = sy / 40;
+
+        // Configure the starting location of the bat
+        // Roughly the middle horizontally
+        mXCoord = mScreenX / 2;
+
+        // The height of the bat
+        // off the bottom of the screen
+        float mYCoord = sy - height;
+
+        // Initialize mRect based on the size and position
+        mRect = new RectF(mXCoord, mYCoord,mXCoord + mLength,mYCoord + height);
+
+        // Configure the speed of the bat
+        // This code means the bat can cover the
+        // width of the screen in 1 second
+        mBatSpeed = mScreenX;
+    }
 }
